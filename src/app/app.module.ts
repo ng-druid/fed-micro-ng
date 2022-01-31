71027bc7340ce9ad502acd91cd3f11477f75698c
druid <angular.druid@gmail.com>
2022-01-30 23:36:18 -0500

    Begin configuring to match traning.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FlightsModule } from './flights/flights.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlightsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/flights/flights-search/flights-search.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "flights-search",
+  templateUrl: `flights-search.component.html`
+})
+export class FlightsSearchComponent {
+}
